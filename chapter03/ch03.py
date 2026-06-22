bcae7423dc06446d008c9857abcc8f442fdbdc93
stygia98 <stygia98@gmail.com>
2026-06-22 18:12:19 +0900

    [Python] chapter03 - 모듈

---
+++ chapter03/ch03.py
@@ -0,0 +1,49 @@
+# 연산자
+# print(3 + 2)
+# print(3 - 2)
+# print(3 * 2)
+# print(3 / 2)
+# print(3 % 2)
+# print(3 // 2)
+# print(3 ** 2)
+# print("*" * 20)
+
+# 조건문
+# num = 12
+# print(2 < num < 10)
+# print(num > 2 and num < 10)
+
+# print(not(2 < num < 10))
+# print(not(num > 2 and num < 10))
+# print(num < 2 or num > 10)
+
+# 연산자
+# print( (5 + 4) * 2 )
+# num = 10
+# num **= 10
+# num //= 10
+# print("num : " + str(num))
+
+# print(abs(-5))
+# print(pow(2,3))
+# print(max(3, 5, 7, 20, 12))
+# print(min(3, 5, 7, 20, 12))
+# print(round(3.141592, 3))
+
+# 모튤 from import
+from math import * 
+# result = floor(4.9999)
+# print(result)
+
+# result = ceil(4.99)
+# print(result)
+
+from random import * 
+
+for i in range(0, 10) :
+  print(int(random()*(179-120+1)+120))
+print()
+
+for i in range(0, 6) :
+  print(randrange(1,45))
+print()
